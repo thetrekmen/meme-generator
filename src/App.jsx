74498419ad88066708b5672541dc38cada0c6793
thetrekmen <jonespm1.dev@gmail.com>
2022-09-23 16:16:52 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header/Header';
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+};
