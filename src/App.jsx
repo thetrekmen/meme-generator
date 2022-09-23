79493257914c8a976c1533e5c06b96553afcf7f4
thetrekmen <jonespm1.dev@gmail.com>
2022-09-23 16:36:25 -0400

    first pass at header

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ import Header from './Components/Header/Header';
 export default function App() {
   return (
     <div className="App">
-      <Header />
+      <Header 
+        trollFace="./src/assets/trollface.png"
+        headerTitle="Meme Generator" 
+        headerTitle2="React Course - Project 3"
+        />
     </div>
   )
 };
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
 
-export default function Header() {
+export default function Header(props) {
     return (
         <div className='header-container' >
-            test
+            <img className='header--img' src={props.trollFace} alt="" />
+            <p className='header--title' >{props.headerTitle}</p>
+            <p className='header--title2' >{props.headerTitle2}</p>
         </div>
     )
 }
