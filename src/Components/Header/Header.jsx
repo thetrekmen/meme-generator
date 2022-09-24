2eb783a6c1236fba0f617333baf93f918a27c4eb
thetrekmen <jonespm1.dev@gmail.com>
2022-09-24 18:56:02 -0400

    updated header font

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,12 +3,12 @@ import './Header.css';
 
 export default function Header(props) {
     return (
-        <div className='header-container' >
+        <header className='header-container' >
             <div className='header--title-container' >
                 <img className='header--img' src={props.trollFace} alt="" />
                 <p className='header--title' >{props.headerTitle}</p>
             </div>
             <p className='header--title2' >{props.headerTitle2}</p>
-        </div>
+        </header>
     )
 }
