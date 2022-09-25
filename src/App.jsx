89cd9d92588d1c890f87953141aeab51e4e30888
thetrekmen <jonespm1.dev@gmail.com>
2022-09-25 01:10:20 -0400

    added a onClick function for props in meme comp

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Meme from './Components/Meme/Meme';
+import memeData from './meme-database'
 
 
 export default function App() {
+
+  function handleOnClick() {
+    return memeData.data.memes[Math.floor(Math.random() * memeData.data.memes.length)].url
+  }
+
   return (
     <div className="App">
       <Header 
@@ -16,6 +22,8 @@ export default function App() {
         placeHolder1="top text"
         placeHolder2="bottom text"
         submitButtonText="Get a new meme image"
+        imgUrl={handleOnClick}
+        state={memeData.data.memes[Math.floor(Math.random() * memeData.data.memes.length)].url}
        />
     </div>
   )
