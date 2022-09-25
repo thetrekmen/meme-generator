ad3145abb4dfbecd11e94c52b17d8404f49170cb
thetrekmen <jonespm1.dev@gmail.com>
2022-09-25 01:09:27 -0400

    added img element and state hooks for changing img

--- src/Components/Meme/Meme.jsx
+++ src/Components/Meme/Meme.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Meme.css';
 
 export default function Meme(props) {
+    const [url,setUrl] = useState(props.state);
+
+    function changeUrl() {
+        setUrl(props.imgUrl);
+    }
+
     return (
         <main>
-            <form action="" >
+            <div className='form' >
                 <div className='textinput-container' >
                     <div className='textinput-containers1 textinput-containers' >
                         <input type="text" id="textinput1" name="textinput1" placeholder={props.placeHolder1} />
@@ -13,8 +19,9 @@ export default function Meme(props) {
                         <input type="text" id="textinput2" name="textinput2" placeholder={props.placeHolder2} />
                     </div>
                 </div>
-                <input className='submit-button' type="submit" value={props.submitButtonText} />
-            </form>
+                <input className='submit-button' type="submit" value={props.submitButtonText} onClick={changeUrl} />
+                <img src={url} alt="" />
+            </div>
         </main>
     )
 }
