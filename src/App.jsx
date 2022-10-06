693a388fbde8def0e229fc75a7c18837c7421cd6
thetrekmen <jonespm1.dev@gmail.com>
2022-10-06 16:36:15 -0400

    added props and added a handle on click function

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,21 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Meme from './Components/Meme/Meme';
-import memeData from './meme-database'
 
 
 export default function App() {
-
-  function handleOnClick() {
-    return memeData.data.memes[Math.floor(Math.random() * memeData.data.memes.length)].url
-  }
-
   return (
     <div className="App">
-      <Header 
+      <Header
         trollFace="./src/assets/trollface.png"
-        headerTitle="Meme Generator" 
+        headerTitle="Meme Generator"
         headerTitle2="React Course - Project 3"
-        />
+      />
       <Meme
         placeHolder1="top text"
         placeHolder2="bottom text"
         submitButtonText="Get a new meme image"
-        imgUrl={handleOnClick}
-        state={memeData.data.memes[Math.floor(Math.random() * memeData.data.memes.length)].url}
-       />
+      />
     </div>
   )
 };
